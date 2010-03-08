da67b68a8a8fb8fa9ed2e2c14852f345937f52cb
vinays <vinays@localhost>
2010-03-08 20:43:47 +0530

    Marker events (minimal)

--- src/com/google/gwt/maps/client/overlay/HasMarker.java
+++ src/com/google/gwt/maps/client/overlay/HasMarker.java
@@ -1,5 +1,24 @@
 package com.google.gwt.maps.client.overlay;
 
 public interface HasMarker extends HasMarkerOptions {
+  
+  public static enum Event {
+    
+    CLICK("click"),
+    DRAG("drag"),
+    DRAG_START("dragstart"),
+    DRAG_ENG("dragend"),
+    ZINDEX_CHANGED("zindex_changed");
+    
+    final private String value;
+    
+    private Event(String value) {
+      this.value = value;
+    }
+    
+    public String getValue() {
+      return this.value;
+    }
+  }
 
 }
