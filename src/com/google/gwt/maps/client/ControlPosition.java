91fa0df105497eb8d562129964d63682bc8f5707
vinays <vinays@localhost>
2010-03-11 14:40:01 +0530

    Documentation for ControlPosition

--- src/com/google/gwt/maps/client/ControlPosition.java
+++ src/com/google/gwt/maps/client/ControlPosition.java
@@ -1,5 +1,20 @@
 package com.google.gwt.maps.client;
 
+/**
+ * Control position types.
+ * <pre>
+ * Layout
+ * =================
+ * |TL     T     TR|
+ * |               |
+ * |L             R|
+ * |               |
+ * |BL     B     BR|
+ * =================
+ * </pre>
+ *
+ * @author vinay.sekhri@gmail.com (Vinay Sekhri)
+ */
 public enum ControlPosition {
 
   BOTTOM, BOTTOM_LEFT, BOTTOM_RIGHT, LEFT, RIGHT, TOP, TOP_LEFT, TOP_RIGHT;
--- src/com/google/gwt/maps/client/impl/ControlPositionImpl.java
+++ src/com/google/gwt/maps/client/impl/ControlPositionImpl.java
@@ -2,6 +2,11 @@ package com.google.gwt.maps.client.impl;
 
 import com.google.gwt.maps.client.ControlPosition;
 
+/**
+ * Get constant's value from JS API.
+ *
+ * @author vinay.sekhri@gmail.com (Vinay Sekhri)
+ */
 public class ControlPositionImpl {
 
   public int getValue(ControlPosition position) {
