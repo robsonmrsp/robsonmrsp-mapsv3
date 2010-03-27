9cb288bdedd9d82aade6de00c56be9e87e0a7c23
vinays <vinays@localhost>
2010-03-27 12:07:38 +0530

    Removed unnecessary imports.

--- src/com/google/gwt/maps/client/geocoder/HasGeocoder.java
+++ src/com/google/gwt/maps/client/geocoder/HasGeocoder.java
@@ -1,7 +1,6 @@
 package com.google.gwt.maps.client.geocoder;
 
 import com.google.gwt.maps.client.HasJso;
-import com.google.gwt.maps.client.geocoder.GeocoderCallback;
 
 public interface HasGeocoder extends HasJso {
   
--- src/com/google/gwt/maps/client/overlay/impl/MarkerOptionsImpl.java
+++ src/com/google/gwt/maps/client/overlay/impl/MarkerOptionsImpl.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.jsio.client.BeanProperties;
 import com.google.gwt.jsio.client.Constructor;
-import com.google.gwt.jsio.client.FieldName;
 import com.google.gwt.jsio.client.JSFlyweightWrapper;
 
 @BeanProperties
