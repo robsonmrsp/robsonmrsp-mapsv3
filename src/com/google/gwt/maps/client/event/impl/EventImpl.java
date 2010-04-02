76d1b80faa5d958b9764122f382f4c44ee72604a
vinays <vinays@localhost>
2010-04-02 14:49:51 +0530

    JavaDocs for event classes.

--- src/com/google/gwt/maps/client/event/impl/EventImpl.java
+++ src/com/google/gwt/maps/client/event/impl/EventImpl.java
@@ -1,9 +1,28 @@
+/* Copyright (c) 2010 Vinay Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.gwt.maps.client.event.impl;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.maps.client.event.EventCallback;
 import com.google.gwt.maps.client.event.MouseEventCallback;
 
+/**
+ * 
+ *
+ * @author vinay.sekhri@gmail.com (Vinay Sekhri)
+ */
 public class EventImpl {
   
   public static EventImpl impl = new EventImpl();
